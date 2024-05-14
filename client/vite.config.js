52180d61ea150b9574452d66c1457fe6eada6898
Rohan <rohangorla07@gmail.com>
2024-05-15 04:27:12 +0530

    debug vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://fullstack-backend-black.vercel.app',
+      "/api":{
+        target: "https://fullstack-backend-black.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/ , "/api"),
+      },
     },
   },
   plugins: [react()],
